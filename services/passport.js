5f1aa23d6aabf57e9bd23f2cfd91b6b7ffe56719
Nicklas Winger <nwi@gecko.no>
2016-12-13 15:11:01 +0100

    Add passport and check if user exists

---
+++ services/passport.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+const config = require('../config');
+
+// Setup options for JWT Strategy
+const jwtOptions = {};
+
+// Create JWT Strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+    // See if the user ID in the payload exists in our database
+    // it it does, call 'done' with that user
+    // otherwise, call done without a user object
+    User.findById(payload.sub, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
+});
+
+// Tell passport to use this Strategy
