f66cde6d666dd24586db92826b22d060c8c4ef95
Nicklas Winger <nwi@gecko.no>
2016-12-13 16:23:41 +0100

    Complete jwtLogin strategy with dummy example for requireAuth on jwt and start local strategy

--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,30 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 const User = require('../models/user');
 const config = require('../config');
 
+// Create local Strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+// Verify username and password, call done with the user
+// if it is the correct username and password
+// otherwise, call done with false
+});
+
 // Setup options for JWT Strategy
-const jwtOptions = {};
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
 
 // Create JWT Strategy
-const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
     // See if the user ID in the payload exists in our database
     // it it does, call 'done' with that user
     // otherwise, call done without a user object
-    User.findById(payload.sub, function(err, user) {
+    User.findById(payload.sub, function (err, user) {
         if (err) {
             return done(err, false);
         }
@@ -26,3 +38,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 // Tell passport to use this Strategy
+passport.use(jwtLogin);
